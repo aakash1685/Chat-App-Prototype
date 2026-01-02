0e245182ed13353077b71a9aa8a11565e15ecff8
aakash1685 <aakashmistry2031@gmail.com>
2026-01-02 23:17:57 +0530

    Initial chat app prototype with socket.io

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 //Socket.io
+
 io.on("connection", (socket) => {
-    console.log("A new user connected", socket.id);
+  socket.on("chat message", (msg) => {
+    socket.emit("chat message", msg);
+});
 });
 
+// io.on("connection", (socket) => {
+//     socket.on("user-message", (message) => {
+//         io.emit("message",message);
+//     })
+// });
+
 app.use(express.static(path.resolve("./public")));
-app.use("/", (req,res) => {
-    return res.sendFile("/public/index.html");
-})
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 server.listen(9000, () => {
-    console.log(`Server is running on PORT 9000`);
-})
+  console.log(`Server is running on PORT 9000`);
+});
